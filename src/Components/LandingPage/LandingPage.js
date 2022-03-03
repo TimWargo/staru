2b2bd2fc61c9fb357b134bfd39de79f0b186f96b
TimWargo <timothywargo@yahoo.com>
2022-03-03 14:44:58 -0500

    Updated the Landing Page

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -4,8 +4,10 @@ class LandingPage extends Component {
     constructor(props) {
         super(props);
     }
-    render() { 
-        return (<h1>Landing Page</h1>);
+    render() {
+        render (
+            <h1>Landing Page</h1>
+        )
     }
 }
  
