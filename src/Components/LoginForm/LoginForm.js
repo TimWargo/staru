1cb640c5ac32fe9da66c4d081eda8b9da206bd9b
Brent Voyles <btv88975@uga.edu>
2022-03-14 18:38:13 -0400

    initial commit

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -5,12 +5,12 @@ class LoginForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            sign: false,
             login: false,
         }
     }
 
     onOpenModal = () => { this.setState({ login: true }); }
-
     onCloseModal = () => { this.setState({ login: false }); }
 
     render() { 
@@ -21,9 +21,15 @@ class LoginForm extends Component {
                 <Modal show={login} onExit={this.onCloseModal} onHide={this.onCloseModal}>
                     <Modal.Header closeButton>
                         <Modal.Title>Login</Modal.Title>
+
                     </Modal.Header>
                     <Modal.Body>
                         <h1>hello</h1>
+                        <p>
+                        <img src="logo192.png" alt="Default Staru Logo" style="float:left;width:42px;height:42px;"></img>
+                        Screen Name:
+                        Password:
+                        </p>
                     </Modal.Body>
                 </Modal>
             </>
