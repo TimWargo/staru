8f88729579118f3f0216297d8e99a3d029a0d945
Brent Voyles <btv88975@uga.edu>
2022-03-18 15:22:10 -0400

    initial login style

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -7,23 +7,22 @@ class LoginForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            login: false,
+            login: false,          
         }
     }
-
+    
     onOpenModal = () => { this.setState({ login: true }); }
     onCloseModal = () => { this.setState({ login: false }); }
-
     render() { 
         const { login } = this.state
         return (
             <>
                 <Nav.Link onClick={this.onOpenModal}>Login</Nav.Link>
-                <Modal show={login} onExit={this.onCloseModal} onHide={this.onCloseModal}>
+                <Modal show={login} onExit={this.onCloseModal} onHide={this.onCloseModal}>                
                     <Modal.Header closeButton>
                         <Modal.Title>Sign in</Modal.Title>
                     </Modal.Header>
-                    <Modal.Body>
+                    <Modal.Body>                                         
                     <div className="form-group">
     <label for="exampleInputEmail1">Email Address</label>
       <input
@@ -41,19 +40,26 @@ class LoginForm extends Component {
         id="exampleInputPassword1"
         placeholder="Password"
       />
-    </div>                                               
-            </Modal.Body>
-                    <Modal.Footer>
-                    <span class="border-top-0"></span>
-                     <div className="div-footer">                                                                                
-                    <button type="button" class="btn btn-outline-secondary">Login</button>                      
-                    <button type="button" class="btn btn-outline-secondary">Forgot Password</button>                                    
-                    </div>
+    </div>                                           
+            </Modal.Body>           
+ <Modal.Footer>           
+ <div className="div-footer2">
+ <button type="button" class="btn btn-outline-primary mr-1">Sign up</button>
+   </div>  
+   <div className="div-footer1">                                                                            
+                    <button type="button" class="btn btn-outline-primary mr-1">Forgot Password?</button>                                  
+                    </div>       
+
+                    <div className="div-footer3">                                                                            
+                    <button type="button" class="btn btn-outline-primary mr-1">Login</button>                                  
+                    </div> 
+
                     </Modal.Footer>
-                </Modal>
-            </>
-        );
-    }
+                </Modal>               
+            </>           
+        );       
+    }   
+
 }
  
 export default LoginForm;
