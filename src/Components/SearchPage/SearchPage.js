78b3ec00b56090c9716f4081ea1ebceb02645295
Brent Voyles <btv88975@uga.edu>
2022-04-13 14:38:05 -0400

    initial commit

--- src/Components/SearchPage/SearchPage.js
+++ src/Components/SearchPage/SearchPage.js
@@ -1,10 +1,63 @@
 import React, { Component } from "react";
+import './SearchPage.css';
+import { Button} from "react-bootstrap";
 
 class SearchPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            keyword: '',
+        }
+    }
+
     render() { 
         return (
             <div className="body">
-                <h1>Search</h1>
+                <div className="searchBar">
+                    <input type='text' className='input' placeholder='Search by Title' />
+                    <Button onClick= {this.onSubmit} variant="primary" size="lg" className="buttModal">
+                    Search
+                    </Button> 
+                </div>
+                <div className= "console-filter">
+                    <Button onClick= {this.onSubmit} variant="primary" size="lg" className="buttModal">
+                    Xbox
+                    </Button>
+                    <Button onClick= {this.onSubmit} variant="primary" size="lg" className="buttModal">
+                    Playstation
+                    </Button>
+                    <Button onClick= {this.onSubmit} variant="primary" size="lg" className="buttModal">
+                    Nintendo
+                    </Button>
+                    <Button onClick= {this.onSubmit} variant="primary" size="lg" className="buttModal">
+                    PC
+                    </Button>
+                </div>
+                <div className= "console-filter">
+                <Button onClick= {this.onSubmit} variant="primary" size="lg" className="buttModal">
+                    Role-Playing
+                    </Button>
+                    <Button onClick= {this.onSubmit} variant="primary" size="lg" className="buttModal">
+                    MMO
+                    </Button>
+                    <Button onClick= {this.onSubmit} variant="primary" size="lg" className="buttModal">
+                    Shooter
+                    </Button>
+                    <Button onClick= {this.onSubmit} variant="primary" size="lg" className="buttModal">
+                    Adventure
+                    </Button>
+                </div>
+                <div className= "console-filter">
+                <Button onClick= {this.onSubmit} variant="primary" size="lg" className="buttModal">
+                    Rated E
+                    </Button>
+                    <Button onClick= {this.onSubmit} variant="primary" size="lg" className="buttModal">
+                    Rated PG-13
+                    </Button>
+                    <Button onClick= {this.onSubmit} variant="primary" size="lg" className="buttModal">
+                    Rated M
+                    </Button>
+                </div>              
             </div>
         );
     }
