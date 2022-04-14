806e9b96261c141226d7c757ed51b61d2087cf04
jackgross18 <jag98307@uga.edu>
2022-04-14 14:16:57 -0400

    minor variable name and styling tweaks

--- src/Components/AboutUsPage/AboutUsPage.js
+++ src/Components/AboutUsPage/AboutUsPage.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
-import Sunset from '../Images/Sunset.jpg'
+import JackPic from '../Images/Sunset.jpg' //change this to pic of Jack
+import AbhiPic from '../Images/Sunset.jpg' //change this to pic of Abhi
+import BrentPic from '../Images/Sunset.jpg' //change this to pic of Brent
+import TimPic from '../Images/Sunset.jpg' //change this to pic of Tim
 
 class AboutUsPage extends Component {
     render() {
@@ -11,41 +14,41 @@ class AboutUsPage extends Component {
                     <div className="row">
                         <div className="col">
                             <div className="card">
-                                <img src={Sunset} className="w-100 border-bottom" alt="Sunset"></img>
+                                <img src={JackPic} className="w-100 border-bottom" alt="Sunset"></img>
                                 <div className="aboutContainer">
                                     <h2>Jack Gross</h2>
                                     <p className="aboutTitle">Floating Wizzard</p>
-                                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                                    <p className="cardText">Some text that describes me lorem ipsum ipsum lorem.</p>
                                 </div>
                             </div>
                         </div>
                         <div className="col">
                             <div className="card">
-                                <img src={Sunset} className="w-100 border-bottom" alt="Sunset"></img>
+                                <img src={AbhiPic} className="w-100 border-bottom" alt="Sunset"></img>
                                 <div className="aboutContainer">
                                     <h2>Abhishek Murugappan</h2>
                                     <p className="aboutTitle">Backend Bodybuilder</p>
-                                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                                    <p className="cardText">Some text that describes me lorem ipsum ipsum lorem.</p>
                                 </div>
                             </div>
                         </div>
                         <div className="col">
                             <div className="card">
-                                <img src={Sunset} className="w-100 border-bottom" alt="Sunset"></img>
+                                <img src={BrentPic} className="w-100 border-bottom" alt="Sunset"></img>
                                 <div className="aboutContainer">
                                     <h2>Brent Voyles</h2>
                                     <p className="aboutTitle">Frontend FlavorTown</p>
-                                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                                    <p className="cardText">Some text that describes me lorem ipsum ipsum lorem.</p>
                                 </div>
                             </div>
                         </div>
                         <div className="col">
                             <div className="card">
-                                <img src={Sunset} className="w-100 border-bottom" alt="Sunset"></img>
+                                <img src={TimPic} className="w-100 border-bottom" alt="Sunset"></img>
                                 <div className="aboutContainer">
                                     <h2>Tim Wargo</h2>
                                     <p className="aboutTitle">Database Guru</p>
-                                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                                    <p className="cardText">Some text that describes me lorem ipsum ipsum lorem.</p>
                                 </div>
                             </div>
                         </div>
