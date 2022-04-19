daf6821bd3bfa969c460ca45f22c04d4a328bb4f
TimWargo <timothywargo@yahoo.com>
2022-04-18 21:46:56 -0400

    Worked on the review component

--- src/Components/ViewGamePage/Review.js
+++ src/Components/ViewGamePage/Review.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
 import { MdStar, MdStarOutline } from 'react-icons/md';
+import { BsChevronUp } from 'react-icons/bs';
 
 class Review extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            showButton: false,
+            isExpanded: true,
+            height: '3.3em',
+        }
         this.renderStars = this.renderStars.bind(this);
+        this.renderDescription = this.renderDescription.bind(this);
+    }
+
+    countLines = () => {
+        
     }
 
     renderStars = () => {
         return (
-            <div>
-                {[...Array(5)].map((star, i) => {                   
+            <div className='d-flex'>
+                {[...Array(5)].map((star, i) => {
+                    if (i < this.props.rating) {
+                        return (
+                            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                                <MdStar style={{fontSize: '0.9vmax', color: 'yellow', zIndex: 2, position: 'absolute'}} />
+                                <MdStar style={{fontSize: '1vmax', color: 'black'}} />
+                            </div>
+                        )
+                    }
                     return (i < this.props.rating) ? <MdStar style={{fontSize: '1vmax', color: 'yellow'}}/> : <MdStarOutline style={{fontSize: '1vmax'}}/>;
                 })}
             </div>
         )
+    }
+
+    renderDescription = () => {
+        if (!this.state.isExpanded) {
+            return <div style={{height: this.state.height}}>{this.props.description}</div>
+        } else {
+            return <Card.Text>{this.props.description}</Card.Text>
+        }
     }
 
     render() {
@@ -29,7 +56,10 @@ class Review extends Component {
                         <Col className='col-auto'  style={{fontSize: '1vmax'}}>#___ ago</Col>
                     </Row>
                     <div className='mb-3' />
-                    <Card.Text style={{fontSize: '1vmax'}}>{this.props.description}</Card.Text>
+                    {this.renderDescription()}
+                    <div className='align-self-end'>
+                        <BsChevronUp />
+                    </div>
                 </Card>
             </Container>
         )
