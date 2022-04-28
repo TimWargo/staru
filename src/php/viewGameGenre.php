944ce094395d52a6f9511aff26e64d068c239435
TimWargo <timothywargo@yahoo.com>
2022-04-28 15:28:50 -0400

    Fixed some issues

--- src/php/viewGameGenre.php
+++ src/php/viewGameGenre.php
@@ -7,7 +7,7 @@ $gameid = $_GET['id'];
 if (isset($gameid) && !empty($gameid)) {
     $query = "SELECT id, name FROM genres INNER JOIN game_genres ON genres.id = game_genres.genre_id WHERE game_id = '$gameid'";
     $result = mysqli_query($db,$query);
-    if ($result->num_rows > 1) {
+    if ($result->num_rows >= 1) {
         echo json_encode($result->fetch_all());
     } else {
         http_response_code(401);
--- src/php/viewOtherGame.php
+++ src/php/viewOtherGame.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Headers: Content-type, Authorization");
 
 $gameid = $_GET['id'];
 if (isset($gameid) && !empty($gameid)) {
-    $query = "SELECT DISTINCT id,title,platform,popularity FROM games INNER JOIN game_genres ON games.id = game_genres.game_id WHERE game_genres.genre_id IN (SELECT genre_id FROM game_genres WHERE game_id = '24') GROUP BY title LIMIT 10";
+    $query = "SELECT DISTINCT id,title,platform,popularity FROM games INNER JOIN game_genres ON games.id = game_genres.game_id WHERE game_genres.genre_id IN (SELECT genre_id FROM game_genres WHERE game_id = '$gameid') GROUP BY title LIMIT 10";
     $result = mysqli_query($db,$query);
     if ($result->num_rows >= 1) {
         echo json_encode($result->fetch_all());
