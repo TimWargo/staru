85fdcd7aaa3d270f7ce0bd9c9eb01b9caa7c9f7a
TimWargo <timothywargo@yahoo.com>
2022-04-27 20:06:56 -0400

    Added php files for functionality

---
+++ src/php/viewOtherGame.php
@@ -0,0 +1,16 @@
+<?php require_once('connect.php');
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-type, Authorization");
+
+$gameid = $_GET['id'];
+if (isset($gameid) && !empty($gameid)) {
+    $query = "SELECT DISTINCT id,title,platform,popularity FROM games INNER JOIN game_genres ON games.id = game_genres.game_id WHERE game_genres.genre_id IN (SELECT genre_id FROM game_genres WHERE game_id = '24') GROUP BY title LIMIT 10";
+    $result = mysqli_query($db,$query);
+    if ($result->num_rows >= 1) {
+        echo json_encode($result->fetch_all());
+    } else {
+        http_response_code(401);
+    }
+}
+?>
