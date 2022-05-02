3d7ce73b82fe135685f21c9b2c9f723a997bbd40
TimWargo <timothywargo@yahoo.com>
2022-05-02 18:07:42 -0400

    Handled the "1 month" versus "2 months" feature

--- src/Components/ViewGamePage/Review.js
+++ src/Components/ViewGamePage/Review.js
@@ -38,23 +38,28 @@ class Review extends Component {
         let seconds = Math.floor((new Date() - this.props.date) / 1000);
         let interval = seconds / 31536000;
         if (interval > 1) {
-            return Math.floor(interval) + " years ago";
+            if (Math.floor(interval) === 1) return Math.floor(interval) + " year ago";
+            else return Math.floor(interval) + " years ago";
         }
         interval = seconds / 2592000;
         if (interval > 1) {
-            return Math.floor(interval) + " months ago";
+            if (Math.floor(interval) === 1) return Math.floor(interval) + " month ago";
+            else return Math.floor(interval) + " months ago";
         }
         interval = seconds / 86400;
         if (interval > 1) {
-            return Math.floor(interval) + " days ago";
+            if (Math.floor(interval) === 1) return Math.floor(interval) + " day ago";
+            else return Math.floor(interval) + " days ago";
         }
         interval = seconds / 3600;
         if (interval > 1) {
-          return Math.floor(interval) + " hours ago";
+            if (Math.floor(interval) === 1) return Math.floor(interval) + " hour ago";
+            else return Math.floor(interval) + " hours ago";
         }
         interval = seconds / 60;
         if (interval > 1) {
-          return Math.floor(interval) + " minutes ago";
+            if (Math.floor(interval) === 1) return Math.floor(interval) + " minute ago";
+            else return Math.floor(interval) + " minutes ago";
         }
         return Math.floor(seconds) + " seconds ago";
     }
